4a81c35392b2dcc9fc26cc563743e677ff7a11ed
Palakurthi Lakshmi Srinivas <130201966+Lakshmisrinivaspalakurthi@users.noreply.github.com>
2026-06-17 15:52:27 +0530

    Create ai_copilot.py

---
+++ backend/services/ai_copilot.py
@@ -0,0 +1,29 @@
+prompt = f"""
+
+Act as a senior recruiter.
+
+Candidate Resume:
+
+{resume}
+
+Target Role:
+
+{role}
+
+Give:
+
+1. ATS Review
+
+2. Missing Skills
+
+3. Resume Improvements
+
+4. Project Recommendations
+
+5. Interview Questions
+
+6. Salary Expectations
+
+7. Learning Plan
+
+"""
