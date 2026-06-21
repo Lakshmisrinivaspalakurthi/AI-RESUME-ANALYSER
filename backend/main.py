5944588b60998a560b837cd2167467623a96233a
Palakurthi Lakshmi Srinivas <130201966+Lakshmisrinivaspalakurthi@users.noreply.github.com>
2026-06-21 20:12:43 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,11 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://ai-resume-analyser-805icr38g.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
